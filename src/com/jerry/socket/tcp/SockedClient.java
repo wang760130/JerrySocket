e1551bb0ab1429ddba5a8992aefe08b3991f8fe4
wangjiajun <wangjiajun@58.com>
2014-11-10 16:21:12 +0800

    first commit

---
+++ src/com/jerry/socket/tcp/SockedClient.java
@@ -0,0 +1,48 @@
+package com.jerry.socket.tcp;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class SockedClient {
+	Socket socket = null;
+	public void sendServer() {
+		try {
+			//创建Socket对象（并连接服务器）
+//			socket = new Socket("127.0.0.1",9000);			
+			socket = new Socket("localhost",9600);
+			
+			//调用getOutputStream方法，进行I/O
+			PrintWriter pw = new PrintWriter(socket.getOutputStream());
+			//向服务器发送消息
+			pw.println("Hello World");
+			pw.println("this is client message");
+			//将数据一次性发出去
+			pw.flush();	
+			
+			//接收服务器返回的数据
+			InputStream is = socket.getInputStream();
+			InputStreamReader reader = new InputStreamReader(is);
+			BufferedReader buffer = new BufferedReader(reader);
+			
+//			BufferedReader br=new BufferedReader(  
+//			         new InputStreamReader(socket.getInputStream()));  
+			String line = buffer.readLine();			
+			System.out.println("服务器端返回的数据："+line+"  ");
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ src/com/jerry/socket/tcp/SocketServer.java
@@ -0,0 +1,60 @@
+package com.jerry.socket.tcp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketServer {
+	ServerSocket serverSocket = null;
+	Socket socket = null;
+	public  void openServer() {
+		try {
+			//创建ServerSocket对象（并绑定端口）
+			serverSocket = new ServerSocket(9600);;
+
+			/**
+			 * 调用accept方法，接收客户端的数据
+			 * 对于accept方法的调用将造成阻塞，直到ServerSocket接受到一个连接请求为止。
+			 * 	一旦连接请求被接受，服务器可以读客户socket中的请求。
+			 */
+			socket = serverSocket.accept();
+			
+			//调用getInputStream方法，接收客户端输出的数据，进行I/O
+			InputStream in = socket.getInputStream();
+			InputStreamReader reader = new InputStreamReader(in);
+			BufferedReader buffer = new BufferedReader(reader);
+			
+//			BufferedReader buffer = new BufferedReader(
+//				new InputStreamReader(socket.getInputStream())
+//			);
+			
+			//输出客户端传过来的数据
+			System.out.println("接受客户端数据：");
+			String line = buffer.readLine();
+			System.out.println(line);			
+			
+			//给客户端传输数据
+			PrintWriter pw = new PrintWriter(socket.getOutputStream());
+			pw.println(line + " form server");
+			pw.flush();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}		
+		}
+	}
+	
+	public static void main(String[] args) {
+		SocketServer socketServer = new SocketServer();
+		socketServer.openServer();
+	}
+}
