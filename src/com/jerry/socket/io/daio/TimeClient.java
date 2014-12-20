d4af50cd3abb07eaa0e8ab65ac6f2f87a42d6cac
JerryWang <jerry002@126.com>
2014-12-20 22:22:33 +0800

    socket

---
+++ src/com/jerry/socket/io/daio/TimeClient.java
@@ -0,0 +1,9 @@
+package com.jerry.socket.io.daio;
+
+public class TimeClient {
+
+    public static void main(String[] args) {
+        int port = 8080;
+        new Thread(new AsyncTimeClientHandler("127.0.0.1", port), "AIOClient").start();
+    }
+}
